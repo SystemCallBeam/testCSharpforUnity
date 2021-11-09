c78c09f9dd3dc004e86cb4bd432c782aea7189df
SystemCallBeam <90573740+SystemCallBeam@users.noreply.github.com>
2021-11-09 20:20:14 +0700

    Add files via upload

---
+++ variable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.position.x);
+
+        if (transform.position.y <= 5f)
+        {
+            Debug.Log("fuckkkkk!!!!");
+        }
+    }
+
+    // Update is called once per frame
+    
+}
