b8c9a8672d06d59f6f026cc73388ab3c196ec292
SystemCallBeam <90573740+SystemCallBeam@users.noreply.github.com>
2021-11-10 19:02:37 +0700

    Update variable.cs
    +forloop

--- variable.cs
+++ variable.cs
@@ -7,6 +7,23 @@ public class variable : MonoBehaviour
     public float coffeetemp = 80.5f;
     float hottemp = 70.0f;
     float coldtemp = 40.0f;
+    
+    private void Start() {
+        string[] str = new string[3];
+        str[0] = "one";
+        str[1] = "two";
+        str[2] = "three";
+
+        foreach (string item in str)
+        {
+            print(item);
+        }
+        int n = 0;
+        for (int i = n; i < 5; i++)
+        {
+            temptest();
+        }
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
